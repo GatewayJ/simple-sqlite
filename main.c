55253e0eedb4aa450a0277d2ed5bb277d972cd31
GatewayJ <835269233@qq.com>
2024-04-23 15:48:25 +0800

    3part

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 typedef struct
 {
@@ -10,6 +11,12 @@ typedef struct
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
+
+
+typedef enum {
+    EXECUTE_SUCCESS,
+    EXCUTE_TABLE_FULL ,
+} ExecuteResult;
 
 typedef enum
 {
@@ -29,10 +36,59 @@ typedef enum
     STATEMEND_SELECT
 } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct
+{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+
 typedef struct
 {
     StatementType type;
+    Row row_to_insert;
 } Statement;
+
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)// todo size_of sizeof
+
+const uint32_t ID_SIZE = size_of_attribute(Row,id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row,username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row,email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + USERNAME_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + EMAIL_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE+USERNAME_SIZE +EMAIL_SIZE;
+
+
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAFGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE*TABLE_MAX_PAFGES;
+
+
+
+typedef struct{
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAFGES]
+} Table;
+
+
+void print_row(Row* row){
+     printf("(%d, %s, %s )\n",row->id,row->username,row->email);
+}
+
+void serialize_row(Row* source,void* destination){
+    memcpy(destination + ID_OFFSET,&(source->id),ID_OFFSET),
+    memcpy(destination + USERNAME_OFFSET,&(source->username),USERNAME_OFFSET),
+    memcpy(destination + EMAIL_OFFSET,&(source->email),EMAIL_OFFSET)
+}
+
+
 
 InputBuffer *new_input_buffer()
 {
