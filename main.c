de58b656619d6a92c99bcfacc79b2a787412d328
root <root@debian.localdomain>
2024-04-27 20:54:02 +0800

    add 2 line commend

--- main.c
+++ main.c
@@ -59,7 +59,7 @@ typedef struct
 const uint32_t ID_SIZE = size_of_attribute(Row,id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row,username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row,email);
-const uint32_t ID_OFFSET = 0;
+const uint32_t ID_OFFSET = 0; //id 字段的开始位置
 const uint32_t USERNAME_OFFSET = ID_OFFSET + USERNAME_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + EMAIL_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE+USERNAME_SIZE +EMAIL_SIZE;
@@ -71,10 +71,10 @@ const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE*TABLE_MAX_PAFGES;
 
 
-
+// 表的内存结构
 typedef struct{
     uint32_t num_rows;
-    void* pages[TABLE_MAX_PAFGES]
+    void* pages[TABLE_MAX_PAFGES];
 } Table;
 
 
@@ -82,10 +82,11 @@ void print_row(Row* row){
      printf("(%d, %s, %s )\n",row->id,row->username,row->email);
 }
 
+// 将标量拷贝到内存的目的位置
 void serialize_row(Row* source,void* destination){
-    memcpy(destination + ID_OFFSET,&(source->id),ID_OFFSET),
+    memcpy(destination + ID_OFFSET,&(source->id),ID_OFFSET), 
     memcpy(destination + USERNAME_OFFSET,&(source->username),USERNAME_OFFSET),
-    memcpy(destination + EMAIL_OFFSET,&(source->email),EMAIL_OFFSET)
+    memcpy(destination + EMAIL_OFFSET,&(source->email),EMAIL_OFFSET);
 }
 
 
